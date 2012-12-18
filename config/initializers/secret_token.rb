4f47261004438870279516e90e3991f82027a0d3
Kevin Tung <kevin@Kevin-Tungs-MacBook.local>
2012-12-17 21:57:47 -0600

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TriangleRaffle::Application.config.secret_token = '7756d309b9b22302825958bc5e3138267ad4eb30823553f4fae462d8576a12515696d792774213a5bd3256e4ab9fa33ceb6cb875f19e5e660d194a08fd57f8e4'
