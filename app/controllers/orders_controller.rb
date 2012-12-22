ea2deb31a4d4c71396edc62799a464ad847b6d0a
Kevin Tung <kevin@Kevin-Tungs-MacBook.local>
2012-12-22 16:00:23 -0600

    add temporary admin authentication

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,14 @@
 class OrdersController < ApplicationController
+  force_ssl
+  
+  
+  if :production == ENV["RAILS_ENV"] 
+    http_basic_authenticate_with :name => "admin", :password => "password"
+  end
+  
   def index
+    puts ENV["RAILS_ENV"]
     @electronic_raffle_orders = ElectronicRaffleOrder.all
   end
+  
 end
