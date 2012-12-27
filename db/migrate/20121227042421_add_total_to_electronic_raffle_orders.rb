05593306ca090038e7caf903794f5740badba273
Kevin Tung <kevin@Kevin-Tungs-MacBook.local>
2012-12-27 02:10:11 -0600

    fix migration script

--- db/migrate/20121227042421_add_total_to_electronic_raffle_orders.rb
+++ db/migrate/20121227042421_add_total_to_electronic_raffle_orders.rb
@@ -1,5 +1,5 @@
 class AddTotalToElectronicRaffleOrders < ActiveRecord::Migration
   def change
-    add_column :electronic_raffle_orders, :total, :decimal, :null => false, :default => BigDecimal('0.00'), :precision => 2, :scale => 8
+    add_column :electronic_raffle_orders, :total, :decimal, :null => false, :default => BigDecimal('0.00'), :precision => 8, :scale => 2
   end
 end
